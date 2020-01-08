27f7934d472cd7011813322b0ab95fbf9e76d417
offcn-java <myaa@qq.com>
2020-01-08 19:21:58 +0800

    新增world file

---
+++ world.java
@@ -0,0 +1,10 @@
+class   {
+	//*************注解****************
+	public static void main(String[] args){
+		System.out.println("hello world");
+
+	}
+	public static void clean(){
+		System.out.println("你好世界");
+	}
+}
